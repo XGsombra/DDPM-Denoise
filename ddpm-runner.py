7e8cbf271365896a0ff4bb273dde2e215ae9c3c3
Xuduo <xuduo.gu@mail.utoronto.ca>
2022-12-05 16:33:18 -0500

    Backup

--- ddpm-runner.py
+++ ddpm-runner.py
@@ -11,8 +11,11 @@ import time
 diffusion = Diffusion.from_pretrained("lsun_church")
 total_time = 0
 noise_type = 'g'
-levels = [0.05, 0.1, 0.2, 0.4]
-level_to_curr_step = {0.05: 15, 0.1: 33, 0.2: 69, 0.4: 120}
+# levels = [0.05, 0.1, 0.2, 0.4]
+# level_to_curr_step = {0.05: 15, 0.1: 33, 0.2: 69, 0.4: 120}
+
+levels = [0.4]
+level_to_curr_step = {0.4: 300}
 for level in levels:
     psnr = 0
     phm = 0
@@ -20,18 +23,19 @@ for level in levels:
     noisy_psnr = 0
     noisy_phm = 0
     noisy_ssim = 0
-    for img_id in range(8, 11):
-        clean = io.imread(f"./samples/clean/validation/{img_id}.jpg").astype(float) / 255
-        noisy_img = io.imread(f"./samples/noisy/validation/{img_id}-{level}-{noise_type}.jpg").astype(float) / 255
+    for img_id in range(11,12):
+        clean = io.imread(f"./samples/clean/ood/{img_id}.jpg").astype(float) / 255
+        # noisy_img = io.imread(f"./samples/noisy/validation/{img_id}-{level}-{noise_type}.jpg").astype(float) / 255
+        noisy_img = clean + level * np.random.randn(256, 256, 3)
         noisy_psnr += calc_psnr(noisy_img, clean)
         noisy_phm += calc_psnr_hvsm(noisy_img, clean)
         noisy_ssim += calc_ssim(noisy_img, clean)
         x = torch.Tensor([noisy_img.transpose([2, 0, 1])]).to(DEVICE)
         curr_step = level_to_curr_step[level]
-        # denoised = diffusion.denoise(1, x=x, curr_step=curr_step, n_steps=curr_step)[0, ...].cpu().detach().numpy().transpose([1,2,0])
-        denoised = io.imread(f"./samples/mprnet-denoised/{img_id}-{level}-{noise_type}.jpg").astype(float) / 255
-        # plt.imshow(denoised)
-        # plt.show()
+        denoised = diffusion.denoise(1, x=x, curr_step=curr_step, n_steps=curr_step)[0, ...].cpu().detach().numpy().transpose([1,2,0])
+        # denoised = io.imread(f"./samples/bilateral-denoised/validation/{img_id}-{level}-{noise_type}.jpg").astype(float) / 255
+        plt.imshow(denoised)
+        plt.show()
         # print(denoised.shape, clean.shape)
         # # total_time += time.time()-start_time
         psnr += calc_psnr(denoised, clean)
